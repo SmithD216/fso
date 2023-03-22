7574bcdbd7854c314773368cae18ee13aab913f1
spindleshanks <smithd216@protonmail.com>
2023-03-22 15:46:59 +0000

    Completed 2.7

--- part2/part2.6/src/App.js
+++ part2/part2.6/src/App.js
@@ -1,8 +1,46 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
+
+    const addName = (e) => {
+        e.preventDefault();
+
+        const nameObject = {
+            name: newName,
+        };
+
+        if (persons.some((e) => e.name === nameObject.name)) {
+            alert("That name has already been entered");
+            return;
+        }
+
+        setPersons(persons.concat(nameObject));
+        setNewName("");
+    };
+
+    const handleChange = (e) => {
+        setNewName(e.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input value={newName} onChange={handleChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map((p) => (
+                <p key={p.name}>{p.name}</p>
+            ))}
+        </div>
+    );
+};
 
 export default App;
