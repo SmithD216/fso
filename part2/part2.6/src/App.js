0587a63e022634d2ce62f3f9c9ced74457e2c94d
spindleshanks <smithd216@protonmail.com>
2023-03-22 15:58:16 +0000

    Finished 2.8

--- part2/part2.6/src/App.js
+++ part2/part2.6/src/App.js
@@ -1,27 +1,41 @@
 import { useState } from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "040-1234567" },
+    ]);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
 
     const addName = (e) => {
         e.preventDefault();
 
         const nameObject = {
             name: newName,
+            number: newNumber,
         };
 
-        if (persons.some((e) => e.name === nameObject.name)) {
-            alert("That name has already been entered");
+        if (
+            persons.some(
+                (e) =>
+                    e.name === nameObject.name || e.number === nameObject.number
+            )
+        ) {
+            alert(`${nameObject.name} is already added to phonebook`);
             return;
         }
 
         setPersons(persons.concat(nameObject));
         setNewName("");
+        setNewNumber("");
     };
 
     const handleChange = (e) => {
         setNewName(e.target.value);
+    };
+
+    const handleNumberChange = (e) => {
+        setNewNumber(e.target.value);
     };
 
     return (
@@ -32,12 +46,18 @@ const App = () => {
                     name: <input value={newName} onChange={handleChange} />
                 </div>
                 <div>
+                    number:
+                    <input value={newNumber} onChange={handleNumberChange} />
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             {persons.map((p) => (
-                <p key={p.name}>{p.name}</p>
+                <p key={p.name}>
+                    {p.name} {p.number}
+                </p>
             ))}
         </div>
     );
